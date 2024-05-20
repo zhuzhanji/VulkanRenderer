7ee1777a492a1c56ad8b928702043b6eef293b81
zhuzhanji <oscarhom@qq.com>
2024-05-20 11:22:47 +0100

    file name

--- VulkanRenderer/KuwaharaFilter.cpp
+++ VulkanRenderer/KuwaharaFilter.cpp
@@ -1499,15 +1499,15 @@ private:
     }
 };
 
-int main() {
-    VulkanRenderer app;
-
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
-}
+//int main() {
+//    VulkanRenderer app;
+//
+//    try {
+//        app.run();
+//    } catch (const std::exception& e) {
+//        std::cerr << e.what() << std::endl;
+//        return EXIT_FAILURE;
+//    }
+//
+//    return EXIT_SUCCESS;
+//}
